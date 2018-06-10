512e5d7fac40682c952cc45446a64fe97d8981a9
Sarah Schaller <sarahcschaller@gmail.com>
2018-06-10 16:43:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function calculatesFarePrice(start, destination) {
 
   if (distance <= 400) {
     return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400)*0.02;
   }
 
   // if distance <= 400, free
