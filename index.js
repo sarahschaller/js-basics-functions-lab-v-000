1bf672e69bbd587895a4323d69db584e5861923a
Sarah Schaller <sarahcschaller@gmail.com>
2018-06-10 16:31:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function distanceFromHqInBlocks (street) {
   return Math.abs(street - 42)
 }
+
+function distanceFromHqInFeet (street) {
+  const blocks = distanceFromHqInBlocks(street);
+
+  return blocks*264;
+}
